4652288795b329e9e8f091f845adac7befa1b0bf
workspacereddy <workspacereddy@gmail.com>
2025-03-15 00:14:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import requests
 
